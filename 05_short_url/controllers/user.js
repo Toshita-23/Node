07aa297fa596bb4812fa17100965aaba03af630b
Toshita-23 <agrawaltoshita@gmail.com>
2024-06-26 18:16:49 +0530

    fsdfsdfs

--- 05_short_url/controllers/user.js
+++ 05_short_url/controllers/user.js
@@ -1,4 +1,4 @@
-const { v4: uuidv4} = require('uuid');
+// const { v4: uuidv4} = require('uuid');
 const User = require('../models/user')
 const {setUser} = require('../service/auth');
 
@@ -16,10 +16,15 @@ async function handleUserSingup(req, res){
 async function handleUserLogin(req, res){
     const {email, password} = req.body;
     const user = await User.findOne({ email, password });
+
     if(!user) return res.render("login", {error: "Invalid Username or Password"});
-    const sessionId = uuidv4();
-    setUser(sessionId, user);
-    res.cookie("uid", sessionId);
+
+    // const sessionId = uuidv4();
+    // setUser(sessionId, user);
+    // res.cookie("uid", sessionId);
+
+    const token = setUser(user);
+    res.cookie("uid", token);
     return res.redirect("/");
 }
 
--- 05_short_url/service/auth.js
+++ 05_short_url/service/auth.js
@@ -1,12 +1,29 @@
-const sessionIdToUserMap = new Map();
+const jwt = require ('jsonwebtoken'); // this is downlaod to make stateless authentication
+const secret = "Toshita@678$23*$"
 
-function setUser(id, user){
-    sessionIdToUserMap.set(id, user);
+function setUser(user){ 
+    return jwt.sign({_id: user._id, email: user.email}, secret); // here we have only user so we used user object as a playload direct we have single value if we have mtliple value then we use like below
+    //   const playload{id, ...user } 
+    //  return jwt.sign(playload, secret);
 }
+function getUser(token){
+        if(!token) return null;
+        try{
+            return jwt.verify(token, secret);
+        } catch{
+            return null;
+        }
+       
+}
+// function setUser(id, user){ // this function is used in statefull authentication
+//     sessionIdToUserMap.set(id, user);
+// }
 
-function getUser(id){
-    return sessionIdToUserMap.get(id);
-}
+
+
+// function getUser(id){
+//     return sessionIdToUserMap.get(id);
+// }
 
 module.exports = {
     setUser,
